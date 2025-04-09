173bc6c1a24183735bf7e7c7bc9389f4cb176254
roalves1234 <roalves1234@hotmail.com>
2025-04-08 22:37:26 -0300

    Endpoint versão

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ llm = LLM(LLM_Model.ChatGPT())
 texto_arquivo = FileTool("Especificacao.txt").load()
 
 @app.get("/pergunta/{pergunta}")
-def read_root(pergunta: str):
+def get_resposta(pergunta: str):
     resposta = llm.set_prompt(f"""
                               Responda em um parágrafo de no máximo 3 linhas a seguinte pergunta: {pergunta} 
 
                               Tendo como base o seguinte texto:    
                               {texto_arquivo}
                               """).go()
-    return {"message": f"Resposta: {resposta}!"}
+    return {"message": f"Resposta: {resposta}"}
+
+    @app.get("/versao")
+    def get_versao():
+        return {"Versão": "001"}
