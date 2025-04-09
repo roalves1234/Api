c4900b44d188855782494a9ea0279cfb6452a067
roalves1234 <roalves1234@hotmail.com>
2025-04-08 21:31:49 -0300

    Pasta Comum

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from Comum.LLM.llm_model import LLM_Model
+from Comum.LLM.llm import LLM
 
 app = FastAPI()
+llm = LLM(LLM_Model.ChatGPT())
 
-@app.get("/hello")
-def read_root():
-    return {"message": "Hello, World!"}
+@app.get("/pergunta/{pergunta}")
+def read_root(pergunta: str):
+    resposta = llm.set_prompt(f"{pergunta}").go()
+    return {"message": f"Resposta:, {resposta}!"}
